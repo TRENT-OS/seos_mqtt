40570dfea75b7bf02086f0bb41c36f4c1cb66003
Felix Schladt <felix.schladt@hensoldt.net>
2024-02-22 15:00:51 +0100

    [DOC] Update Copyright
    Change files to GPLv2 or Commercial licensing

--- include/MQTTseL4.h
+++ include/MQTTseL4.h
@@ -1,7 +1,11 @@
 /*
- *  MQTT wrapper
+ * MQTT wrapper
  *
- *  Copyright (C) 2018-2021, HENSOLDT Cyber GmbH
+ * Copyright (C) 2018-2024, HENSOLDT Cyber GmbH
+ * 
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * For commercial licensing, contact: info.cyber@hensoldt.net
  */
 
 #pragma once
